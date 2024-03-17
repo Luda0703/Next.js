79f7a65543f92e9cce6942840d194f1171d459e5
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-03-17 13:53:38 +0200

    Client Components

---
+++ src/app/components/input-field.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+import { Field } from 'formik';
+
+export interface InputFieldProps {
+  label?: string,
+  id: any,
+}
+
+export default function InputFieldProps({label, id, ...rest }: InputFieldProps) {
+  return (
+    <div className='flex flex-col'>
+        {label && 
+        <label htmlFor={id} className='mb-2 text-base color-greay-900'>{label}
+        </label>}
+        <Field {...rest} id={id} className='p-3 h-11 text-sm rounded border border-gray-300 shadow'/>
+    </div>
+  );
+}
