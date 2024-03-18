4954e4516c5dbeeaeeded70960ff4b10e9fe0cc8
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-03-18 17:39:54 +0200

    Intercepting Routes

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,28 +1,24 @@
-import AddCompanyButton from "../../components/add-company-button";
-import CompanyTable from "../../components/company-label";
-import CompanyRow from "../../components/company-row";
-import Header from "../../components/header";
-import SearchInput from "../../components/search-input";
-import Toolbar from "../../components/toolbar";
+
+import React from 'react';
+import CompanyRow from '@/app/components/company-row';
+import CompanyTable from '@/app/components/company-label';
 import { Status } from '@/app/components/status-label';
 
-export default function Page() {
-    return (
-    <>
-      <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton/>}>
-        <SearchInput/>
-      </Toolbar>
-      <CompanyTable>
-        <CompanyRow 
-            id={1}
-            category="Products"
-            company="Costco"
-            status={Status.Pending}
-            promotion={true}
-            country="USA"
-            joinedDate="02.19.2023"/>
-      </CompanyTable>
-    </>
-    );
-  }
+export interface PageProps {}
+
+export default async function Page({}: PageProps) {
+
+  return (
+    <CompanyTable>
+      <CompanyRow
+        id={1}
+        category="Products"
+        company="Costco"
+        status={Status.Pending}
+        promotion={true}
+        country="USA"
+        joinedDate="02.19.2023"
+      />
+    </CompanyTable>
+  );
+}
--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,20 +1,17 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@/app/components/button';
-import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
 
-const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
-  ssr: false,
-})
+
 
 export default function AddCompanyButton() {
-    const [show, setShow] = useState(false);
+    const router = useRouter();
 
   return (
     <>
-    <Button onClick={() => setShow(true)}>Add company</Button>
-    <CompanyFormModal onSubmit={console.log} show={show} onClose={() => setShow(false)}/>
+    <Button onClick={() => router.push('./companies/new')}>Add company</Button>
     </>
   );
 }
