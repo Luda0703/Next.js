cd7b6a9eef1fcea2e50795c7bcf133538d9a2dc1
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-03-18 11:32:34 +0200

    Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import AddCompanyButton from '@/app/components/add-company-button';
-import ClientComponent from './components/client-component';
-import ServerComponent from './components/server-component';
-import ServerComponentCopy from './components/server-component-copy';
 
 export default function Page() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
-      <ServerComponent/>
-      <ClientComponent><ServerComponentCopy/></ClientComponent>
     </main>
   );
 }
