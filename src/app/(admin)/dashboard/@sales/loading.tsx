a991ecf86926e8b2265d8fe9c658a12fc8303e7f
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-03-18 16:42:22 +0200

    Parallel Routes

---
+++ src/app/(admin)/dashboard/@sales/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface LoadingProps {}
+
+export default async function Loading({}: LoadingProps) {
+
+  return (
+    <div>Loading...</div>
+  );
+}
--- src/app/(admin)/dashboard/newCompanies/page.tsx
+++ src/app/(admin)/dashboard/newCompanies/page.tsx
@@ -3,7 +3,8 @@ import Header from "@/app/components/header";
 export default function Page() {
   return (
     <>
-    <Header>Dashboard</Header>
+    <Header>Dashboard / New companies</Header>
+    <div>Some aditional info</div>
   </>
   );
 }
