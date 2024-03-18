8e0dc9198c1b962e23ae2318cb5eccdac325a1f7
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-03-18 11:54:56 +0200

    Error Handling

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import MagicButton from "@/app/components/magic-button";
 
 export default function Page() {
   return (
     <main>
       <h1 className="text-xl">Dashboard page</h1>
+      <MagicButton/>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import AddCompanyButton from '@/app/components/add-company-button';
+import MagicButton from './components/magic-button';
 
 export default function Page() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
+      <MagicButton/>
     </main>
   );
 }
