e0a068d7fffd4dd7690c17a0f02762b4a1c31f4c
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-03-19 10:01:08 +0200

    Data fetching

--- src/app/(admin)/dashboard/@promotions/page.tsx
+++ src/app/(admin)/dashboard/@promotions/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getSummaryPromotions } from '@/lib/api';
+import { getPromotions} from '@/lib/api';
 import SummaryTableHeader from '@/app/components/summary-table-header';
 import DashboardCard from '@/app/components/dashboard-card';
 import SummaryTable from '@/app/components/summary-table';
@@ -8,7 +8,7 @@ import SummaryTableCell from '@/app/components/summary-table-cell';
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-  const data = await getSummaryPromotions();
+  const data = await getPromotions();
 
   return (
     <DashboardCard label="Promotions">
@@ -21,10 +21,10 @@ export default async function Page({}: PageProps) {
           </>
         }
       >
-        {data.map(({ promotionId, promotionName, companyTitle, discount }) => (
-          <tr key={promotionId}>
+        {data.map(({ id, title, companyTitle, discount }) => (
+          <tr key={id}>
             <SummaryTableCell>{companyTitle}</SummaryTableCell>
-            <SummaryTableCell>{promotionName}</SummaryTableCell>
+            <SummaryTableCell>{title}</SummaryTableCell>
             <SummaryTableCell align="center">{`-${discount}%`}</SummaryTableCell>
           </tr>
         ))}
