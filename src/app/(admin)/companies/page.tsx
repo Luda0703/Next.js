5009c4523d97a10fc3315c38dc38212e65f414ed
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-03-19 10:54:55 +0200

    react query

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,24 +1,25 @@
 
 import React from 'react';
-import CompanyRow from '@/app/components/company-row';
-import CompanyTable from '@/app/components/company-label';
-import { Status } from '@/app/components/status-label';
+import CompanyTable from '@/app/components/company-tabel';
+import getQueryClient from '@/lib/utils/getQueryClient';
+import { getCompanies } from '@/lib/api';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
+  const queryClient = getQueryClient();
 
+  await queryClient.prefetchQuery({
+    queryKey: ['companies'],
+    queryFn: () => getCompanies({ cache: 'no-store' }),
+    staleTime: 10 * 1000,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
   return (
-    <CompanyTable>
-      <CompanyRow
-        id={1}
-        category="Products"
-        company="Costco"
-        status={Status.Pending}
-        promotion={true}
-        country="USA"
-        joinedDate="02.19.2023"
-      />
-    </CompanyTable>
+    <HydrationBoundary state={dehydratedState}>
+    <CompanyTable />
+  </HydrationBoundary>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import AddCompanyButton from '@/app/components/add-company-button';
-
 export default function Page() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <AddCompanyButton />
     </main>
   );
 }
